600a661ab3d03aed267aefe0955307352e94dd24
Ansh <kh.anshul04@gmail.com>
2024-02-01 23:39:30 +0530

    V4

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,37 @@ const moviesSchema = new mongoose.Schema({
 })
 
 const movies = mongoose.model('Movies',moviesSchema);
+const topMovies = mongoose.model('TopMovies',moviesSchema);
 
 app.get('/movies',async(req,res)=>{
    await movies.find({}).then((data)=>{
+    if(data && data.length !== 0){
+        res.send(data).status(200);
+       }else{
+        res.send("No Data Found").status(404);
+       }
+   }).catch((err)=>{
+    console.log(err);
+    res.send(err).status(403);
+   });
+   
+})
+
+app.get('/movie/:id',async(req,res)=>{
+   const movieId = req.params.id;
+   if(movieId){
+      await topMovies.findOne({_id : movieId}).then((data)=>{
+         res.send(data).status(200);
+      }).catch((err)=>{
+         res.send("Error At Backend").status(404);
+      })
+   }else{
+      res.send("No Data Found").status(404);
+   }
+})
+
+app.get('/top_movies',async(req,res)=>{
+   await topMovies.find({}).then((data)=>{
     if(data && data.length !== 0){
         res.send(data).status(200);
        }else{
@@ -59,6 +87,27 @@ app.post('/admin/movies',authenticate,async(req,res)=>{
         bg_poster_url :newMovie.bg_poster_url || null
      }
      const putMovie = new movies(movieData);
+      await putMovie.save();
+      res.send("Posted Success").status(200);
+   }else{
+    res.send("invalid request").status(403);
+   }
+   
+})
+
+app.post('/admin/top_movies',authenticate,async(req,res)=>{
+   const newMovie = req.body;
+   if(newMovie || newMovie.length >0){
+    const movieData = {
+        name : newMovie.name || null,
+        description : newMovie.description || null,
+        movie_url : newMovie.movie_url || null,
+        rating : newMovie.rating || null,
+        poster_url :newMovie.poster_url ||null,
+        trailer_url :newMovie.trailer_url || null,
+        bg_poster_url :newMovie.bg_poster_url || null
+     }
+     const putMovie = new topMovies(movieData);
       await putMovie.save();
       res.send("Posted Success").status(200);
    }else{
---
+++ src/components/Slideshow.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+const spanStyle = {
+  padding: '20px',
+  background: '#efefef',
+  color: '#000000'
+}
+
+const divStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundSize: 'cover',
+  height: '400px'
+}
+
+
+const Slideshow = ({images}) => {
+    return (
+      <div className="slide-container">
+        <Slide>
+         {images.map((slideImage, index)=> (
+            <div key={index}>
+              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+      </div>
+    )
+}
+
+export default Slideshow;
