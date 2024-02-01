594850b446b4c4819bb921cf1ff43752eb550724
Ritik Sharma <ritiksharma6230@gmail.com>
2024-02-01 15:22:56 +0530

    Hover-Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
       <div className='flex justify-between gap-96 px-10 ' >
         <div className=' md:flex font-bold text-2xl hidden ' >Gem Cinema</div>
         <div className='flex gap-10 font-bold text-2xl ' >
-          <Link>Home</Link>
-          <Link>Movies</Link>
-          <Link>About</Link>
+          <Link className='hover:text-red-500' > Home </Link>
+          <Link className='hover:text-red-500' >Movies</Link>
+          <Link className='hover:text-red-500'>About</Link>
 
         </div>
 
