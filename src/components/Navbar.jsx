c3b696006a45ae829829afb8046769e6d614c854
Ritik Sharma <ritiksharma6230@gmail.com>
2024-02-01 15:13:19 +0530

    Frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between gap-96 px-10 mt-5 ' >
-      <div className=' md:flex font-bold text-2xl hidden ' >Gem Cinema</div>
-      <div className='flex gap-10 font-bold text-2xl ' >
-        <Link>Home</Link>
-        <Link>Movies</Link>
-        <Link>About</Link>
+    <div className='absolute z-20 w-full'>
+
+      <div className='flex justify-between gap-96 px-10 ' >
+        <div className=' md:flex font-bold text-2xl hidden ' >Gem Cinema</div>
+        <div className='flex gap-10 font-bold text-2xl ' >
+          <Link>Home</Link>
+          <Link>Movies</Link>
+          <Link>About</Link>
+
+        </div>
 
       </div>
-
     </div>
   )
 }
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+import Button from '@mui/material/Button';
+import Navbar from '../components/Navbar';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff1706',
+    },
+
+  },
+});
 
 const Home = () => {
   return (
-    <div>
+  <>
    
+    <div className='flex justify-center items-center h-screen gap-20 bg-gradient-to-r from-cyan-500 to-blue-500' >
+        
+      <div className='flex flex-col p-20 md:w-9/12 '>
+        <h1 className='font-bold text-8xl' >Wakanda Forever</h1>
+        <h3 className='font-bold text-lg mt-5 ' >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt tenetur animi fugiat sit quidem, fuga architecto <br></br>blanditiis reprehenderit repellat totam quos fugit corporis enim aliquid amet, quasi harum nostrum.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus quisquam provident distinctio facere fuga ea autem incidunt! Incidunt assumenda, ratione eveniet, laborum neque, consectetur distinctio quos totam ipsum reprehenderit quod!
+        </h3>
+        <div className='flex gap-20 mt-10' >
+          <ThemeProvider theme={theme}>
+            <Button size='large' variant="contained">Watch Now</Button>
+            <Button size='large' variant="outlined" >
+              Watch Trailer
+            </Button>
+          </ThemeProvider>
+        </div>
+      </div>
+      <div className='md:flex hidden' >
+        <img className='bg-cover bg-center size-2/3 rounded-3xl' src="https://lumiere-a.akamaihd.net/v1/images/pp_disney_blackpanther_wakandaforever_1289_d3419b8f.jpeg" alt="" srcset="" />
+      </div>
     </div>
+    </>
   )
 }
 
 export default Home
+
