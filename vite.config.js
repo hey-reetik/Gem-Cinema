37c15d4e13e50e29e75e8c912556706c832e8cc9
Ansh <kh.anshul04@gmail.com>
2024-02-01 11:42:04 +0530

    Initial Setup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host:"0.0.0.0",
+  },
+  resolve:{
+    alias:{
+      "@components" : "/src/components",
+      "@containers" : "/src/containers",
+      "@assets": "/src/assets"
+    }
+  },
+  
 })
